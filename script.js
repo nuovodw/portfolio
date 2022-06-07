2d4f965c5effce7b98253c68ffa0081501174704
Daniel Won <68261298+nuovodw@users.noreply.github.com>
2022-06-07 09:40:06 -0500

    edit js

--- script.js
+++ script.js
@@ -9,3 +9,13 @@ navList.forEach((navLink) => {
 		checkbox.checked = false;
 	});
 });
+
+window['optimizely'] = window['optimizely'] || [];
+window['optimizely'].push({
+  type: "event",
+  eventName: "custom1",
+  tags: {
+    revenue: 0, // Optional in cents as integer (500 == $5.00)
+    value: 0.00 // Optional as float
+  }
+});
